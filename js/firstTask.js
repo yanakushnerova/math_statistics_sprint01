db5fc44d23f8ba61278eacd4f8bd860f8473f154
Yana Kushnerova <ykushnerov@c1r7p10.khpi.ucode-connect.study>
2021-05-23 17:06:02 +0300

    task 1 main calculations

--- js/firstTask.js
+++ js/firstTask.js
@@ -88,32 +88,110 @@ function checkHypotesis(arr) {
 
     let table = document.createElement("table")
     table.innerHTML = "<th>Xi</th>\
-    <th>Xi+1</th>\
-    <th>ni</th>\
-    <th>x1</th>\
-    <th>x2</th>\
-    <th>F(x1)</th>\
-    <th>F(x2)</th>\
-    <th>pi</th>\
-    <th>ni'</th>\
-    <th>Ki'</th>"
+                    <th>Xi+1</th>\
+                    <th>ni</th>\
+                    <th>Zi</th>\
+                    <th>Zi+1</th>\
+                    <th>F(x1)</th>\
+                    <th>F(x2)</th>\
+                    <th>pi</th>\
+                    <th>ni'</th>\
+                    <th>Ki'</th>"
     table.id = "task1_table"
+
+    let series = []
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i][2]; j++) {
+            series.push(parseFloat(((arr[i][0] + arr[i][1]) / 2).toFixed(4)))
+        }
+    }
+
+    let mean_value = parseFloat((series.reduce((a, b) => a + b) / series.length).toFixed(5))
+    let dispersion_value = 0
+
+    for (let i = 0; i < series.length; i++) {
+        dispersion_value += Math.pow(series[i] - mean_value, 2)
+    }
+    
+    dispersion_value = parseFloat((dispersion_value / (series.length - 1)).toFixed(5))
+    let deviation_value = parseFloat(Math.sqrt(dispersion_value).toFixed(5))
 
     for (let i = 0; i < 9; i++) {
         let tr = document.createElement("tr")
 
         let xi = document.createElement("td")
         xi.innerText = arr[i][0]
+
         let xi1 = document.createElement("td")
         xi1.innerText = arr[i][1]
+
         let ni = document.createElement("td")
         ni.innerText = arr[i][2]
+
+        let zi = document.createElement("td")
+        let zi_value = ((arr[i][0] - mean_value) / deviation_value).toFixed(5)
+        zi.innerText = zi_value
+
+        let zi1 = document.createElement("td")
+        let zi1_value = ((arr[i][1] - mean_value) / deviation_value).toFixed(5)
+        zi1.innerText = zi1_value
+
+        let fzi = document.createElement("td")
+        let fzi_value = 0
+        if (zi_value < 0) {
+            fzi_value = - (laplaceIntegration(parseFloat(Math.abs(zi_value)))).toFixed(5)
+        } else {
+            fzi_value = (laplaceIntegration(parseFloat(Math.abs(zi_value)))).toFixed(5)
+        }
+        fzi.innerText = fzi_value
+
+        let fzi1 = document.createElement("td")
+        let fzi1_value = 0
+        if (zi1_value < 0) {
+            fzi1_value = - (laplaceIntegration(parseFloat(Math.abs(zi1_value)))).toFixed(5)
+        } else {
+            fzi1_value = (laplaceIntegration(parseFloat(Math.abs(zi1_value)))).toFixed(5)
+        }
+        fzi1.innerText = fzi1_value
+
+        let pi = document.createElement("td")
+        let pi_value = fzi1_value - fzi_value
+        pi.innerText = pi_value.toFixed(5)
+
+        let npi = document.createElement("td")
+        let npi_value = series.length * pi_value
+        npi.innerText = npi_value.toFixed(5)
+
+        let ki = document.createElement("td")
+        let ki_value = ((arr[i][2] - npi_value) ** 2) / npi_value
+        ki.innerText = ki_value.toFixed(5)
 
         tr.appendChild(xi)
         tr.appendChild(xi1)
         tr.appendChild(ni)
+        tr.appendChild(zi)
+        tr.appendChild(zi1)
+        tr.appendChild(fzi)
+        tr.appendChild(fzi1)
+        tr.appendChild(pi)
+        tr.appendChild(npi)
+        tr.appendChild(ki)
         table.appendChild(tr)
     }
 
     document.getElementById("task1_solution").appendChild(table)
+
+    let description2 = document.createElement("p")
+    description2.innerHTML = "Степінь свободи:"
+    document.getElementById("task1_solution").appendChild(description2)
+
+    let k_value = arr.length - 3
+    let k = document.createElement("div")
+    k.innerText = k_value
+    document.getElementById("task1_solution").appendChild(k)
+
+    let description3 = document.createElement("p")
+    description3.innerHTML = "Спостережене значення:"
+    document.getElementById("task1_solution").appendChild(description3)
 }
